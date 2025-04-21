6b37d95f4ff9f9f5c44e3a57564d0e46a6acd84f
Harrison Hubbard <hhubg04@gmail.com>
2025-04-21 13:28:21 -0400

    Updated Item class to include quantity, setters,getters and methods.

--- src/main/java/VendingItem.java
+++ src/main/java/VendingItem.java
@@ -1,14 +1,106 @@
 /**
+ * VendingItem.java
  * @author Miles Hudock
  * CSC-331-002
  * Wed Apr 16th
  */
+
 public class VendingItem {
     private String name;
     private double price;
+    private int quantity;
 
-    public VendingItem(String name, double price)   {
+    /**
+     * Constructor for VendingItem.
+     * @param name      The name of the item.
+     * @param price     The price of the item
+     * @param quantity   The quantity of the item
+     */
+    public VendingItem(String name, double price ,int quantity)   {
         this.name = name;
         this.price = price;
+        this.quantity = 0;
     }
+
+    // Setters
+
+    /**
+     * Sets the name of the item.
+     * @param name The name of the item.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Sets the price of the item.
+     * @param price the price of the item
+     */
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    /**
+     * Sets the quantity of the item.
+     * @param quantity the quantity of the item
+     */
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    // Getters
+
+    /**
+     * Gets the name of the item
+     * @return the name of the item
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Gets the price of the item.
+     * @return the price of the item
+     */
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    // Methods
+
+    /**
+     * Decreases the quantity of the item by one.
+     */
+    public void decrementQuantity(){
+        this.quantity = quantity - 1;
+    }
+
+    /**
+     * Increases the quantity of the item by one.
+     */
+     public void incrementQuantity(){
+        this.quantity = quantity + 1;
+     }
+
+    /**
+     * Checks if the item is sold out.
+     * @return True if the item is sold out, false otherwise.
+     */
+     public boolean isSoldOut(){
+         return this.quantity <= 0;
+     }
+
+    /**
+     * Checks if the item is available.
+     * @return True if the item is available, false otherwise.
+     */
+     public boolean isAvailable(){
+        return this.quantity > 0;
+     }
+
+
 }
